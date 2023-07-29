ea7d9f9ce4e5fb8f4bfb4bdd7c6833b660a588cd
Otlwaetse Ramoswaane <ramoswaane@gmail.com>
2023-07-29 16:07:37 +0200

    deployment on render.com

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -48,7 +48,7 @@ const Customizer = () => {
      try{
       setGeneratingImg(true);
 
-      const response = await fetch('http://localhost:3001/api/v1/dalle', {
+      const response = await fetch('https://shirtserver.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers:{
           'Content-Type': 'application/json'
